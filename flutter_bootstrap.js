095a3bd05f42bf41fbf42a44180c81d5042a478a
HardikArgon <hardik.argon@gmail.com>
2025-09-08 18:13:41 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2282630518"
+    serviceWorkerVersion: "2371904150"
   }
 });
